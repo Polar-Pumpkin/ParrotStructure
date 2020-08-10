0931673560a058493918ce425254726e4cc84253
EntityParrot_ <entityparrot@qq.com>
2020-08-10 17:45:27 +0800

    [+] 修复了一些问题
    - 异步判断失败，只能暂时先主线程判断，以后再想办法解决。

--- src/main/java/org/serverct/parrot/parrotstructure/ParrotStructure.java
+++ src/main/java/org/serverct/parrot/parrotstructure/ParrotStructure.java
@@ -6,12 +6,12 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.serverct.parrot.parrotstructure.data.Structure;
 import org.serverct.parrot.parrotstructure.listener.StructureListener;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
 public final class ParrotStructure extends JavaPlugin {
 
@@ -33,16 +33,28 @@ public final class ParrotStructure extends JavaPlugin {
         Bukkit.getScheduler().cancelTasks(this);
     }
 
-    public Structure match(Block block, Material material) {
-        AtomicReference<Structure> result = new AtomicReference<>();
-        Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
-            for (Structure structure : structureMap.values()) {
-                if (structure.match(block, material)) {
-                    result.set(structure);
+    public Structure matchAsync(Block block, Material material) {
+        final Structure[] result = {null};
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Structure structure : structureMap.values()) {
+                    if (structure.match(block, material)) {
+                        result[0] = structure;
+                    }
                 }
+                result[0] = null;
             }
-            result.set(null);
-        });
-        return result.get();
+        }.runTaskAsynchronously(this);
+        return result[0];
+    }
+
+    public Structure match(Block block, Material material) {
+        for (Structure structure : structureMap.values()) {
+            if (structure.match(block, material)) {
+                return structure;
+            }
+        }
+        return null;
     }
 }
