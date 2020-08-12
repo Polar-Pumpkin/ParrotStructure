1fb1fc06e094bdd29d47e04820e1e3c0c9622767
EntityParrot_ <entityparrot@qq.com>
2020-08-12 17:08:46 +0800

    [+] 新增了钥匙和其他结构成分设置。

--- src/main/java/org/serverct/parrot/parrotstructure/data/Structure.java
+++ src/main/java/org/serverct/parrot/parrotstructure/data/Structure.java
@@ -1,15 +1,14 @@
 package org.serverct.parrot.parrotstructure.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.serverct.parrot.parrotstructure.ParrotStructure;
 
 import java.util.*;
@@ -21,6 +20,7 @@ abstract class Structure {
 
     private final NamespacedKey key;
     private final List<StructurePart> parts;
+    private final ItemStack activeKey;
 
     public abstract void onInteract(PlayerInteractEvent event);
 
@@ -36,43 +36,80 @@ abstract class Structure {
         UUID user = event.getPlayer().getUniqueId();
         if (!cooldownMap.getOrDefault(user, false)) {
             cooldownMap.put(user, true);
+            ItemStack hold = event.getItem();
+
+            if (activeKey != null) {
+                if (hold == null) {
+                    return;
+                }
+                if (!activeKey.isSimilar(hold)) {
+                    return;
+                }
+                hold.setAmount(hold.getAmount() - 1);
+            }
+
             onInteract(event);
-            Bukkit.getScheduler().runTaskLaterAsynchronously(ParrotStructure.getInstance(), () -> {
-                cooldownMap.put(user, false);
-            }, 10L);
+
+            Block clicked = event.getClickedBlock();
+            if (clicked != null) {
+                Location center = clicked.getLocation();
+                StructurePart trigger = getTrigger(clicked, event.getAction());
+
+                if (trigger != null) {
+                    this.parts.forEach(part -> {
+                        Location target = part.getLocation().center(trigger.getLocation()).get(center);
+                        if (part.disappear) {
+                            target.getBlock().setType(Material.AIR);
+                        } else if (part.broke) {
+                            target.getBlock().breakNaturally();
+                        }
+                        if (part.explode > 0) {
+                            World world = target.getWorld();
+                            if (world != null) {
+                                world.createExplosion(target, part.explode);
+                            }
+                        }
+                    });
+                }
+            }
+
+            Bukkit.getScheduler().runTaskLaterAsynchronously(ParrotStructure.getInstance(), () -> cooldownMap.put(user, false), 10L);
         }
     }
 
     public boolean isInteract(PlayerInteractEvent event) {
         Block block = event.getClickedBlock();
         if (block == null) return false;
-        for (StructurePart part : getTrigger()) {
-            if (part.getMaterial() == block.getType()) {
-                Action action = event.getAction();
-                switch (part.getType()) {
-                    case LEFT:
-                        if (action == Action.LEFT_CLICK_BLOCK) {
-                            return true;
-                        }
-                    case RIGHT:
-                        if (action == Action.RIGHT_CLICK_BLOCK) {
-                            return true;
-                        }
-                    case BOTH: {
-                        if (action == Action.LEFT_CLICK_BLOCK || action == Action.RIGHT_CLICK_BLOCK) {
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
+        return getTrigger(block, event.getAction()) != null;
     }
 
     public List<StructurePart> getTrigger() {
         List<StructurePart> target = new ArrayList<>(parts);
         target.removeIf(part -> part.getType() == StructurePart.TriggerType.NONE);
         return target;
+    }
+
+    public StructurePart getTrigger(Block block, Action action) {
+        for (StructurePart part : getTrigger()) {
+            if (part.getMaterial() == block.getType()) {
+                switch (part.getType()) {
+                    case LEFT:
+                        if (action == Action.LEFT_CLICK_BLOCK) {
+                            return part;
+                        }
+                    case RIGHT:
+                        if (action == Action.RIGHT_CLICK_BLOCK) {
+                            return part;
+                        }
+                    case BOTH: {
+                        if (action == Action.LEFT_CLICK_BLOCK || action == Action.RIGHT_CLICK_BLOCK) {
+                            return part;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     public boolean match(Block block, Material material) {
@@ -92,10 +129,20 @@ abstract class Structure {
     }
 
     public @Data
+    @AllArgsConstructor
     static class StructurePart {
         private final Material material;
         private final TriggerType type;
         private final RelativeLocation location;
+        private boolean disappear = false;
+        private float explode = -1;
+        private boolean broke = false;
+
+        public StructurePart(Material material, TriggerType type, RelativeLocation location) {
+            this.material = material;
+            this.type = type;
+            this.location = location;
+        }
 
         public enum TriggerType {
             BOTH, LEFT, RIGHT, NONE
